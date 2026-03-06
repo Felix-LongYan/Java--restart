299b3a9f902b0bfec0f4bbfdf9d2d575598b1858
Felix-LongYan <longminjian111@gmail.com>
2026-03-06 20:19:08 +0800

    chapter 1 test

---
+++ src/chapter1/Looptest.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Looptest {
+    public static void main(String[]args){
+        int x =1;
+        while (x<=10){
+            System.out.println("number: "+ x);
+            x++;
+        }
+    }
+}
